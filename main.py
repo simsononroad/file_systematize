5ac0f7fe460643b6d13d51fad9f6863600d583cf
simsononroad <d-github@kopaszegy.hu>
2025-01-01 18:47:48 +0100

    replace files

--- main.py
+++ main.py
@@ -8,14 +8,38 @@ courrent_os = platform.system()
 
 def linux():
     pass
-
+py = ".py"
 
 def windows():
     path = Path.cwd()
     file = os.listdir(path)
-    
+
     for files in file:
-        print(files)
+
+        if ".py" in files:
+            try:
+                os.mkdir("python")
+            except FileExistsError:
+                pass
+            dest = "python"
+            file_p_des = f"{dest}/{files}"
+            os.rename(files, file_p_des)
+        elif ".png" in files or ".jpg" in files:
+            os.mkdir("kepek")
+            dest = "kepek"
+            file_p_des = f"{dest}/{files}"
+            os.rename(files, file_p_des)
+        elif ".json" in files:
+            os.mkdir("json")
+            dest = "json"
+            file_p_des = f"{dest}/{files}"
+            os.rename(files, file_p_des)
+        elif ".pdf" in files:
+            os.mkdir("pdf")
+            dest = "pdf"
+            file_p_des = f"{dest}/{files}"
+            os.rename(files, file_p_des)
+
 
 if courrent_os == "Windows":
     windows()
